8d491908a749754b182c46f821f71a92f9f39fd3
Airtoyt <airtoyt@gmail.com>
2025-02-24 21:53:05 +0300

    Update Airtoytsbotv1.py

--- Airtoytsbotv1.py
+++ Airtoytsbotv1.py
@@ -1,15 +1,17 @@
 import telebot
-    
-    # Замени 'TOKEN' на токен твоего бота
-    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+
 bot = telebot.TeleBot("8021362694:AAGum8YyJEuojz1ylBRfVIxBSMPgWd5g0es")
-    
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я твой Telegram бот - повторюшка. Напиши что-нибудь!")
-    
+
+@bot.message_handler(commands=['repeat', 'hello'])
+def repeat_message(message):
+    bot.reply_to(message, 'Теперь я буду повторять за тобой!')
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-    
-bot.polling().py
+
+bot.polling()
